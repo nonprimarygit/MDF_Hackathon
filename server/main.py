05a3648047c9ecb5f5dcbccd41391c24aee9f975
Dimitri Zhorzholiani <dmtzhorzholiani@gmail.com>
2024-05-29 14:48:12 +0400

    Updates

--- server/main.py
+++ server/main.py
@@ -15,10 +15,8 @@ from bs4 import BeautifulSoup
 from urllib.parse import urlparse
 from PIL import Image
 from io import BytesIO
-
-# Deepfake
-from kernel_utils import VideoReader, FaceExtractor, confident_strategy, predict_on_video_set
-from classifiers import DeepFakeClassifier
+#from kernel_utils import VideoReader, FaceExtractor, confident_strategy, predict_on_video_set
+#from classifiers import DeepFakeClassifier
 from pytube import YouTube
 
 import pytesseract
@@ -28,18 +26,36 @@ import requests
 import re
 import json
 import uuid
-import torch
 
 import pandas as pd
+from translate import Translator
 
-client = OpenAI(api_key="OPENAI TOKEN")
+client = OpenAI(api_key="OPENAI")
 
-os.environ['GOOGLE_CSE_ID'] = os.environ.get('GOOGLE_CSE_ID', 'GOOGLE SEARCH CSE ID')
-os.environ['GOOGLE_API_KEY'] = os.environ.get('GOOGLE_API_KEY', 'GOOGLE SEARCH API KEY')
+os.environ['GOOGLE_CSE_ID'] = os.environ.get('GOOGLE_CSE_ID', 'CSE')
+os.environ['GOOGLE_API_KEY'] = os.environ.get('GOOGLE_API_KEY', 'API')
 
 search = GoogleSearchAPIWrapper()
 
-# Helper function - It was faster/easier to do everything is one file, please forgive me...
+# Helper function - It was faster/easier to do everything is one folder, please forgive me...
+def chunk_text(text, chunk_size):
+    return [text[i:i+chunk_size] for i in range(0, len(text), chunk_size)]
+
+def translate_to_georgian(text):
+    chunk_size = 500
+    chunks = chunk_text(text, chunk_size)
+    translator = Translator(to_lang='ka')
+
+    translated_chunks = [translator.translate(chunk) for chunk in chunks]
+    return ''.join(translated_chunks)
+
+def translate_to_english(text):
+    chunk_size = 500
+    chunks = chunk_text(text, chunk_size)
+    translator = Translator(to_lang='en')
+    translated_chunks = [translator.translate(chunk) for chunk in chunks]
+    return ''.join(translated_chunks)
+
 def download_video(video_url, target_dir):
     if "youtube.com" in video_url or "youtu.be" in video_url:
         # Handle YouTube URL
@@ -64,10 +80,8 @@ def download_video(video_url, target_dir):
 
 # Prediction function
 def predict_video(video_url, test_dir):
-    # Download video to the test directory
     video_path = download_video(video_url, test_dir)
 
-    # Set up the parameters for prediction
     frames_per_video = 32
     video_reader = VideoReader()
     video_read_fn = lambda x: video_reader.read_frames(x, num_frames=frames_per_video)
@@ -75,10 +89,8 @@ def predict_video(video_url, test_dir):
     input_size = 380
     strategy = confident_strategy
 
-    # Since there is only one video, create a list with just that video's name
     test_videos = [os.path.basename(video_path)]
 
-    # Run the prediction on the video
     predictions = predict_on_video_set(face_extractor=face_extractor, input_size=input_size, models=models,
                                        strategy=strategy, frames_per_video=frames_per_video, videos=test_videos,
                                        num_workers=6, test_dir=test_dir)
@@ -89,101 +101,44 @@ def predict_video(video_url, test_dir):
 
 # Weights import from DVR (Deep video recognition)
 
-weights_dir = "weights"
-model_names = [
-   "final_111_DeepFakeClassifier_tf_efficientnet_b7_ns_0_36",
-   "final_555_DeepFakeClassifier_tf_efficientnet_b7_ns_0_19",
-    "final_777_DeepFakeClassifier_tf_efficientnet_b7_ns_0_29",
-    "final_777_DeepFakeClassifier_tf_efficientnet_b7_ns_0_31",
-    "final_888_DeepFakeClassifier_tf_efficientnet_b7_ns_0_37",
-    "final_888_DeepFakeClassifier_tf_efficientnet_b7_ns_0_40",
-    "final_999_DeepFakeClassifier_tf_efficientnet_b7_ns_0_23",
-]
+#weights_dir = "weights"
+#model_names = [
+#    "final_111_DeepFakeClassifier_tf_efficientnet_b7_ns_0_36",
+#    "final_555_DeepFakeClassifier_tf_efficientnet_b7_ns_0_19",
+    # "final_777_DeepFakeClassifier_tf_efficientnet_b7_ns_0_29",
+    # "final_777_DeepFakeClassifier_tf_efficientnet_b7_ns_0_31",
+    # "final_888_DeepFakeClassifier_tf_efficientnet_b7_ns_0_37",
+    # "final_888_DeepFakeClassifier_tf_efficientnet_b7_ns_0_40",
+    # "final_999_DeepFakeClassifier_tf_efficientnet_b7_ns_0_23",
+#]
 
-models = []
-for model_name in model_names:
-   path = os.path.join(weights_dir, model_name)
-   model = DeepFakeClassifier(encoder="tf_efficientnet_b7_ns").to("cuda")
-   print("loading state dict {}".format(path))
-   checkpoint = torch.load(path, map_location="cpu")
-   state_dict = checkpoint.get("state_dict", checkpoint)
-   model.load_state_dict({re.sub("^module.", "", k): v for k, v in state_dict.items()}, strict=True)
-   model.eval()
-   del checkpoint
-   models.append(model.half())
+#models = []
+#for model_name in model_names:
+#    path = os.path.join(weights_dir, model_name)
+#    model = DeepFakeClassifier(encoder="tf_efficientnet_b7_ns").to("cuda")
+#    print("loading state dict {}".format(path))
+#    checkpoint = torch.load(path, map_location="cpu")
+#    state_dict = checkpoint.get("state_dict", checkpoint)
+#    model.load_state_dict({re.sub("^module.", "", k): v for k, v in state_dict.items()}, strict=True)
+#    model.eval()
+#    del checkpoint
+#    models.append(model.half())
 
 app = Flask(__name__)
 CORS(app)
 
 @app.route('/api/fact', methods=['POST'])
 def receive_fact():
-    # Parse JSON data from request
     data = request.get_json()
-    print(data)
     if 'data' in data and 'url' in data and 'contentType' in data:
         url = data['url']
         content_type = data['contentType']
-        domain = urlparse(url).netloc
-
-        response = requests.get(url)
-        html_content = response.text
-
-        soup = BeautifulSoup(html_content, 'html.parser')
-        title = soup.title.string
-
-        # Checking SSL
-        ssl_url = "https://ssl-certificate-checker2.p.rapidapi.com/ssl-certificate-checker/check"
-
-        ssl_params = {"host": domain}
-        ssl_headers = {
-            "X-RapidAPI-Key": "XAPIKEY",
-	        "X-RapidAPI-Host": "XAPIHOST"
-        }
-
-        ssl_response = requests.get(ssl_url, headers=ssl_headers, params=ssl_params)
-
-        if ssl_response.status_code == 200:
-            ssl_data = ssl_response.json()
-
-            subject = ssl_data['subject']
-            issuer = ssl_data['issuer']
-            validFrom = ssl_data['validFrom']
-            validTo = ssl_data['validTo']
-            expiresInDays = ssl_data['expiresInDays']
-            fingerprint = ssl_data['fingerprint']
-            fingerprint256 = ssl_data['fingerprint256']
-            serialNumber = ssl_data['serialNumber']
-            pem = ssl_data['pem']
-            protocol = ssl_data['protocol']
-            cipher = ssl_data['cipher']
-            subjectAltNames = ssl_data['subjectAltNames']
-            infoAccess = ssl_data['infoAccess']
-
-        # DA PA & Spam score detection
-        da_pa_url = "https://da-pa-and-spam-score-api.p.rapidapi.com/check-url"
-
-        da_pa_payload = { "url": domain }
-        da_pa_headers = {
-            "content-type": "application/json",
-            "Content-Type": "application/json",
-            "X-RapidAPI-Key": "XAPIKEY",
-            "X-RapidAPI-Host": "XAPIHOST"
-        }
-
-        da_pa_response = requests.post(da_pa_url, json=da_pa_payload, headers=da_pa_headers)
-
-        if da_pa_response.status_code == 200:
-            da_pa_data = da_pa_response.json()
-
-            daScore = da_pa_data[0].get('DA', 'Unknown')
-            paScore = da_pa_data[0].get('PA', 'Unknown')
-            spamScore = da_pa_data[0].get('Spam Score', 'Unknown')
 
         if content_type == 'text':
             text = data['data']
 
             response = client.chat.completions.create(
-                model="gpt-4-1106-preview",
+                model="gpt-4o",
                 response_format={"type": "json_object"},
                 messages=[
                     {"role": "system", "content": f"NOTE: You need to formulate questions like `Ronaldos current football team`. Current date: {datetime.datetime.now()}. You are smart AI, your goal is to generate google search query based on chunk of text to determine this text is true or not. NOTE: formulate question to determine it's true or no. never directly use date, and text. Always formulate questions. return ouput as json"},
@@ -196,30 +151,33 @@ def receive_fact():
             output = json.loads(response.choices[0].message.content)
             if output['query']:
                 searched_data = search.run(output['query'])
+
                 evaluation_string = f"""
                                     Text to check: {text}
-                                    Google output: {searched_data}PA Score: {paScore} | DA Score: {daScore} | Spam Score: {spamScore}
-                                    PA Score: {paScore} | DA Score: {daScore} | Spam Score: {spamScore}
-                                    SSL Information:
-                                    Issuer: {issuer} | Subject: {subject} | ValidFrom: {validFrom} | ValidTo: {validTo}
-                                    Domain: {domain} | Title: {title}
+                                    Google output: {searched_data}
+
+                                    Note: return strings in original language (Georgian or English) 
+                                    Note: Keys must be in english only, just values might change
                                     """
 
-                # Ask GPT to evaluate based on provided data
+
                 evaluation_request = client.chat.completions.create(
-                    model="gpt-4-1106-preview",
+                    model="gpt-4o",
                     response_format={"type": "json_object"},
                     messages=[
-                            {"role": "system", "content": f"Current date: {datetime.datetime.now()}. You are FACT checker AI. your goal is to check if provided text from image is true or no based on various factors. You need to return outpus as json with keys - `valid` - It's boolean, `real_data` - It's optional string or nonetype. `reason`. - String. If valid is False you need to return real fact. If it's unknown return nonetype. In any case we need reason why it's either true or no. Note: Mostly you need to analyse text and google search text because high DA/PA can lie. NOTE: if information is false, valid must uqual to False"},
+                            {"role": "system", "content": f"Current date: {datetime.datetime.now()}. You are FACT checker AI. your goal is to check if provided text from image is true or no based on various factors. You need to return outpus as json with keys - `valid` - It's boolean, `real_data` - It's optional string or nonetype. `reason`. - String. If valid is False you need to return real fact. If it's unknown return nonetype. In any case we need reason why it's either true or no. Note: Mostly you need to analyse text and google search text because high DA/PA can lie. NOTE: if information is false, valid must uqual to False."},
                             {"role": "user", "content": evaluation_string},
                     ]
                 )
+                
                 evaluation_output = json.loads(evaluation_request.choices[0].message.content)
-
                 if evaluation_output['valid']:
-                    return jsonify({'type': 'true', 'reason': evaluation_output['reason']})
+                    return jsonify({'type': 'true', 'reason': translate_to_georgian(evaluation_output['reason']).lower()})
                 else:
-                    return jsonify({'type': 'false', 'message': evaluation_output['real_data'], 'reason': evaluation_output['reason']})
+                    if evaluation_output['real_data']:  
+                        return jsonify({'type': 'false', 'message': translate_to_georgian(evaluation_output['real_data']).lower(), 'reason': translate_to_georgian(evaluation_output['reason']).lower()})
+                    else:
+                        return jsonify({'type': 'true', 'reason': translate_to_georgian(evaluation_output['reason']).lower()})
             else:
                 return jsonify({'status': 'error', 'message': 'error while extracting query from text'})
         if content_type == 'image':
@@ -252,7 +210,7 @@ def receive_fact():
                 extracted_response = response.choices[0].message.content
                 json_string = re.search(r'```json\n(.*)\n```', extracted_response, re.DOTALL).group(1)
                 json_object = json.loads(json_string)
-                print(json_object['question'])
+
                 if json_object['question']:
                     searched_data = search.run(json_object['question'])
 
@@ -260,9 +218,7 @@ def receive_fact():
                                     Overview of image: {json_object['information']}
                                     Google output: {searched_data}
                                     """
-                    print(evaluation_string)
 
-                # Ask GPT to evaluate based on provided data
                     evaluation_request = client.chat.completions.create(
                         model="gpt-4-1106-preview",
                         response_format={"type": "json_object"},
@@ -272,11 +228,11 @@ def receive_fact():
                         ]
                     )
                     evaluation_output = json.loads(evaluation_request.choices[0].message.content)
-                    print(evaluation_output)
+
                     if evaluation_output['valid']:
-                        return jsonify({'type': 'true', 'reason': evaluation_output['reason']})
+                        return jsonify({'type': 'true', 'reason': evaluation_output['reason'].lower()})
                     else:
-                        return jsonify({'type': 'false', 'message': evaluation_output['real_data'], 'reason': evaluation_output['reason']})
+                        return jsonify({'type': 'false', 'message': evaluation_output['real_data'].lower(), 'reason': evaluation_output['reason'].lower()})
                 else:
                     return jsonify({'status': 'error', 'message': 'error while extracting query from text'})
 
@@ -343,17 +299,14 @@ def receive_fact():
             )
 
             output = json.loads(response.choices[0].message.content)
-            print(output)
             if output['query']:
                 searched_data = search.run(output['query'])
 
                 evaluation_string = f"""
                                     Text to check: {text}
                                     Google output: {searched_data}
-                                    PA Score: {paScore} | DA Score: {daScore} | Spam Score: {spamScore}
                                     """
 
-                print(evaluation_string)
 
                 evaluation_request = client.chat.completions.create(
                     model="gpt-4-1106-preview",
